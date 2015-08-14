414c85aa76f643bea59f540ab514cf9757941629
Chris White <Chris2.White@colostate.edu>
2015-08-14 09:14:24 -0600

    Proof-of-concept printing non-customized labels

---
+++ source/javascripts/all.js
@@ -0,0 +1,39 @@
+//= require_tree .
+
+$(document).ready(function() {
+  var labelXML = "";
+
+  $.get("/javascripts/StudentNametag.label", function(data) {
+    labelXML = data;
+  });
+
+  var printers = dymo.label.framework.getPrinters();
+  var printerName = "";
+  for (var i = 0; i < printers.length; ++i)
+  {
+      var printer = printers[i];
+      if (printer.printerType == "LabelWriterPrinter")
+      {
+          printerName = printer.name;
+          break;
+      }
+  }
+
+  $('#print').click(function() {
+    if (labelXML === "") {
+      return false;
+    }
+
+    var label = dymo.label.framework.openLabelXml(labelXML);
+
+    if (printerName === "") {
+      alert('No Printers Available. If you have a printer attached, please try restarting your computer.');
+      return false;
+    }
+
+    label.print(printerName)
+
+    console.log('hi!');
+    return false;
+  });
+});
